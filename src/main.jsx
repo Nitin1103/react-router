c36f31610b04a796ae2e0df9e4e9b8a4100ef5e1
Nitin1103 <redmi1smail@gmail.com>
2024-06-17 00:04:15 +0530

    fixed deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact.jsx'
-import User from './components/User/User.jsx'
-import Github from './components/Github/Github.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import User from './components/User/User.jsx';
+import Github from './components/Github/Github.jsx';
 
+const basename = 'react-router';
 
-// creating a browser router
-// 1st way: using createBrowserRouter function
 const myrouter = createBrowserRouter([
   {
     path: '/',
@@ -43,10 +42,10 @@ const myrouter = createBrowserRouter([
       }
     ]
   }
-])
+], { basename });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={myrouter} />
   </React.StrictMode>,
-)
+);
